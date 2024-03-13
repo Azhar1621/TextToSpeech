7602e9f0ee5a8827334e31084d1be57be3cd796b
Azhar Khan <azar123khan456@gmail.com>
2024-03-14 01:50:08 +0530

    added voice to text button

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let speech = new SpeechSynthesisUtterance();
 
 let voices = [];
-
+let textarea = document.querySelector("textarea");
 let voiceSelect = document.querySelector("select");
 
 window.speechSynthesis.onvoiceschanged = () => {
@@ -15,7 +15,29 @@ voiceSelect.addEventListener("change", () => {
     speech.voice = voices[voiceSelect.value];
 })
 
-document.querySelector("button").addEventListener("click", () => {
+document.querySelector("#listen").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
 });
+
+click_to_record.addEventListener('click',function(){
+    var speech = true;
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('')
+
+            textarea.innerHTML = transcript;
+        console.log(transcript);
+    });
+    
+    if (speech == true) {
+        recognition.start();
+    }
+})
