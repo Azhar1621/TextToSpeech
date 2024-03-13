1f2bae3d088d22c7c33c9cabe58a85e422dd7f16
Azhar Khan <azar123khan456@gmail.com>
2024-03-14 01:56:24 +0530

    Refactor code for voice recognition and speech synthesis

--- app.js
+++ app.js
@@ -5,39 +5,41 @@ let textarea = document.querySelector("textarea");
 let voiceSelect = document.querySelector("select");
 
 window.speechSynthesis.onvoiceschanged = () => {
-    voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0];
+  voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[0];
 
-    voices.forEach((voice,i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+  voices.forEach(
+    (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
+  );
 };
 
 voiceSelect.addEventListener("change", () => {
-    speech.voice = voices[voiceSelect.value];
-})
-
-document.querySelector("#listen").addEventListener("click", () => {
-    speech.text = document.querySelector("textarea").value;
-    window.speechSynthesis.speak(speech);
+  speech.voice = voices[voiceSelect.value];
 });
 
-click_to_record.addEventListener('click',function(){
-    var speech = true;
-    window.SpeechRecognition = window.webkitSpeechRecognition;
+document.querySelector("#listen").addEventListener("click", () => {
+  speech.text = document.querySelector("textarea").value;
+  window.speechSynthesis.speak(speech);
+});
 
-    const recognition = new SpeechRecognition();
-    recognition.interimResults = true;
+click_to_record.addEventListener("click", function () {
+  var speech = true;
+  window.SpeechRecognition = window.webkitSpeechRecognition;
 
-    recognition.addEventListener('result', e => {
-        const transcript = Array.from(e.results)
-            .map(result => result[0])
-            .map(result => result.transcript)
-            .join('')
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;
 
-            textarea.innerHTML = transcript;
-        console.log(transcript);
-    });
-    
-    if (speech == true) {
-        recognition.start();
-    }
-})
+  recognition.addEventListener("result", (e) => {
+    const transcript = Array.from(e.results)
+      .map((result) => result[0])
+      .map((result) => result.transcript)
+      .join("");
+
+    textarea.innerHTML = transcript;
+    console.log(transcript);
+  });
+
+  if (speech == true) {
+    recognition.start();
+  }
+});
